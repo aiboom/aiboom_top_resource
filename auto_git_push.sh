7ec48c2b7c155c7d72ab7e7f95ca2e07b53847b2
Langsky <1015681552@qq.com>
2019-03-24 13:51:07 +0800

    add picture

--- auto_git_push.sh
+++ auto_git_push.sh
@@ -1,6 +1,5 @@
 # !/bin/bash
-export SYNC_DIR=/home/lansky/Pictures/aiboom_top_resource
-cd $SYNC_DIR
+cd ~/Pictures/aiboom_top_resource
 git add .
 git commit -m "add picture"
 git push origin master
